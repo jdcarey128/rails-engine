9e858ec7050c285a5318b1cd14f378b7e0a19e8a
Joshua Carey <jdcarey128@gmail.com>
2020-12-15 19:00:16 -0700

    Refactor merchant and invoice factories

--- spec/factories/invoices.rb
+++ spec/factories/invoices.rb
@@ -3,5 +3,15 @@ FactoryBot.define do
     customer 
     merchant 
     status { 'Shipped' }
+
+    trait :with_invoice_items do 
+      transient do 
+        invoice_item_count { 3 }
+      end
+
+      after :created do |invoice, evaluator|
+        invoice.invoice_items << create_list(:invoice_item, evaluator.invoice_item_count)
+      end
+    end
   end
 end
--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -11,5 +11,23 @@ FactoryBot.define do
         create_list(:item, evaluator.item_count, merchant: merchant)
       end
     end
+
+    trait :with_invoice_items do
+      transient do 
+        invoice_count { 3 }
+        item_quantity { 1 }
+        unit_price { 10 }
+        transaction_result { 'success' }
+      end 
+
+      after :create do |merchant, evaluator|
+        invoices = create_list(:invoice, evaluator.invoice_count, merchant: merchant)
+        invoices.each do |invoice|
+          invoice.invoice_items << create_list(:invoice_item, evaluator.invoice_count, quantity: evaluator.item_quantity, unit_price: evaluator.unit_price)
+          invoice.transactions << create(:transaction, result: evaluator.transaction_result)
+        end
+      end
+    end
+    
   end
 end
