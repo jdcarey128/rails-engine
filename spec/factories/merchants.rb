57e05addebd18d08cd0454f73274868ad74b4a8a
Joshua Carey <jdcarey128@gmail.com>
2020-12-16 16:26:30 -0700

    Add created_at transient to merchants with invoice items

--- spec/factories/merchants.rb
+++ spec/factories/merchants.rb
@@ -16,14 +16,15 @@ FactoryBot.define do
       transient do 
         invoice_count { 1 }
         invoice_status { 'shipped' }
-        invoice_item_count { 3 }
+        invoice_item_count { 1 }
         item_quantity { 3 }
         unit_price { 10 }
         transaction_result { 'success' }
+        created_at {'2020-12-01 12:00:00'}
       end 
 
       after :create do |merchant, evaluator|
-        invoices = create_list(:invoice, evaluator.invoice_count, merchant: merchant, status: evaluator.invoice_status)
+        invoices = create_list(:invoice, evaluator.invoice_count, merchant: merchant, status: evaluator.invoice_status, created_at: evaluator.created_at)
         invoices.each do |invoice|
           invoice.invoice_items << create_list(:invoice_item, evaluator.invoice_item_count, quantity: evaluator.item_quantity, unit_price: evaluator.unit_price)
           invoice.transactions << create(:transaction, result: evaluator.transaction_result)
