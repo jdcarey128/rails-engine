1d9f71949415e17eb6e53147c8fdeb3ee5ec6ae0
Joshua Carey <jdcarey128@gmail.com>
2020-12-16 19:19:14 -0700

    Move revenue controller to business controller

--- app/controllers/api/v1/merchants/business_controller.rb
+++ app/controllers/api/v1/merchants/business_controller.rb
@@ -10,6 +10,18 @@ class Api::V1::Merchants::BusinessController < ApplicationController
     render json: MerchantSerializer.new(Merchant.most_items(params[:quantity]))
   end
 
+  def total_revenue
+    start_date = params[:start]
+    end_date = params[:end]
+    unless start_date && end_date
+      errors = []
+      errors << 'start date' unless start_date
+      errors << 'end date' unless end_date
+      return missing_params(errors)
+    end
+    render json: RevenueSerializer.format_revenue(Api::V1::MerchantRevenueFacade.total_revenue(start_date, end_date))
+  end
+
   private 
     def missing_params(params)
       render json: {
